d0e0f0abbc100532bdfccb358aa0aee4100a942f
Yataro Fujinaga <yatarofujinaga@gmail.com>
2020-08-24 11:45:39 +0900

    validationの日本語化

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'あなたのパスワードはリセットされています',
+    'sent' => 'パスワードリセットのメールを送りました',
+    'throttled' => 'ログインに続けて失敗しています。再度お試しください',
+    'token' => 'パスワードリセットトークンが無効です',
+    'user' => "そのメールアドレスを持つユーザーを見つけられませんでした",
+
+];
